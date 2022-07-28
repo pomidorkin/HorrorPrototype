61020572b065ff7c9c963cd190ca53737eb874f0
Pomidorkin <pbriuzgin@gmail.com>
2022-07-28 13:36:16 +0900

    Camera clamp added

--- Assets/Scripts/MouseLook.cs
+++ Assets/Scripts/MouseLook.cs
@@ -9,26 +9,28 @@ public class MouseLook : MonoBehaviour
     public float mouseSensitivity = 0.1f;
     [SerializeField] Transform playerBody;
     PlyerInputActions plyerInputActions;
-    float xRotation = 0f;
-
-    // NIPS TEST
     private Vector2 MouseMoveInput;
+    float xRot;
 
-    // NIPS TEST
+    [Header("Camera clamp settings")]
+    [Range(-90, 0)]
+    [SerializeField] float cameraMaxLook = -60f;
+    [Range(0, 90)]
+    [SerializeField] float cameraMinLook = 60f;
+
     private void Awake()
     {
         plyerInputActions = new PlyerInputActions();
         plyerInputActions.Player.Enable();
         plyerInputActions.Player.Look.performed += Look;
     }
-
-    // NIPS TEST
-    float xRot;
+    
     private void Look(InputAction.CallbackContext obj)
     {
         Vector2 NonNormalizedDelta = MouseMoveInput * .5f * .1f;
 
         xRot -= NonNormalizedDelta.y * mouseSensitivity;
+        xRot = Mathf.Clamp(xRot, cameraMaxLook, cameraMinLook);
 
         playerBody.Rotate(0f, NonNormalizedDelta.x * mouseSensitivity, 0f);
         transform.localRotation = Quaternion.Euler(xRot, 0f, 0f);
@@ -38,25 +40,9 @@ public class MouseLook : MonoBehaviour
     {
         MouseMoveInput = plyerInputActions.Player.Look.ReadValue<Vector2>();
     }
-
-    // end NIPS TEST
     void Start()
     {
         Cursor.lockState = CursorLockMode.Locked;
-        // NIPS TEST
 
     }
-
-    // Update is called once per frame
-   /* void Update()
-    {
-        float mouseX = Mouse.current.delta.x.ReadValue() * mouseSensitivity * Time.deltaTime;
-        float mouseY = Mouse.current.delta.y.ReadValue() * mouseSensitivity * Time.deltaTime;
-
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
-        playerBody.Rotate(Vector3.up * mouseX);
-    }*/
 }
