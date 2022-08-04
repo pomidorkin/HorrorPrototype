0d97014b5f8c41065c56295fb73ebf808ffa1b54
Pomidorkin <pbriuzgin@gmail.com>
2022-08-04 22:39:03 +0900

    AI animation & path finding added

--- Assets/Scripts/AI/AiLocomotion.cs
+++ Assets/Scripts/AI/AiLocomotion.cs
@@ -6,8 +6,12 @@ using UnityEngine.AI;
 public class AiLocomotion : MonoBehaviour
 {
     NavMeshAgent agent;
-    //[SerializeField] Animator animator;
+    [SerializeField] Animator animator;
     [SerializeField] Transform playerTransform;
+    [SerializeField] float maxTime = 1f;
+    [SerializeField] float maxDistance = 1f;
+
+    float timer = 0;
 
     void Start()
     {
@@ -16,15 +20,27 @@ public class AiLocomotion : MonoBehaviour
 
     void Update()
     {
-        agent.destination = playerTransform.position;
-
-        /*if (agent.hasPath)
+        timer -= Time.deltaTime;
+        if (timer < 0f)
         {
-            animator.SetFloat("Speed", agent.velocity.magnitude);
+            float sqDistance = (playerTransform.position - agent.destination).sqrMagnitude;
+            if (sqDistance > maxDistance * maxDistance)
+            {
+                agent.destination = playerTransform.position;
+            }
+            timer = maxTime;
+            
+        }
+
+        //animator.SetFloat("Speed", /*agent.velocity.magnitude*/ agent.speed);
+
+        if (agent.hasPath)
+        {
+            animator.SetFloat("Speed", /*agent.velocity.magnitude*/ agent.desiredVelocity.magnitude);
         }
         else
         {
             animator.SetFloat("Speed", 0);
-        }*/
+        }
     }
 }
--- Assets/Scripts/AudioVisualization/MyParamCube.cs
+++ Assets/Scripts/AudioVisualization/MyParamCube.cs
@@ -8,11 +8,9 @@ public class MyParamCube : MonoBehaviour
     [SerializeField] [Range(0f, 10f)] float animTriggerValue = 1.6f;
     void Update()
     {
-        // My Code
         if (AudioPeer.bandBuffer[band] > animTriggerValue)
         {
-            // Commented out for testing
-            //gameObject.GetComponent<Animator>().Play("SlammAnimation");
+            gameObject.GetComponent<Animator>().Play("SlammAnimation");
         }
 
     }
